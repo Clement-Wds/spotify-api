3c02dc2516173893b9e4d6b9440acbf8ef01243d
Clément Wds <71884576+Clement-Wds@users.noreply.github.com>
2023-11-16 10:39:15 +0100

    debug port server

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ const db = mysql.createConnection({
   database: process.env.DB_DATABASE,
 });
 
-// Middleware d'erreur global
+// // Middleware d'erreur global
 app.use((err, req, res, next) => {
   res.status(500).send('Erreur : ' + err.message);
 });
 
-// Connexion à la base de données
+// // Connexion à la base de données
 db.connect(err => {
   if (err) {
     // Appel du middleware d'erreur global avec l'erreur
@@ -37,9 +37,7 @@ app.use('/api', routes); // Toutes les routes seront préfixées par /api
 
 // Synchronisez les modèles avec la base de données
 sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${port}`);
-  });
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
 
 app.listen(port, () => {
